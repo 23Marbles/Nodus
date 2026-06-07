f572cc7292b44e4e0403cb48d4809fa0ef42b130
23 Marbles <eth8n.ryan@gmail.com>
2026-06-07 17:49:26 +0100

    add display to ids

--- crates/nodus-graph/src/sets/edge.rs
+++ crates/nodus-graph/src/sets/edge.rs
@@ -1,2 +1,10 @@
+use std::fmt::Display;
+
 #[derive(Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct EdgeId(pub u16);
+
+impl Display for EdgeId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        <u16 as Display>::fmt(&self.0, f)
+    }
+}
--- crates/nodus-graph/src/sets/vertex.rs
+++ crates/nodus-graph/src/sets/vertex.rs
@@ -1,5 +1,13 @@
+use std::fmt::Display;
+
 #[derive(Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct NodeId(pub u16);
+
+impl Display for NodeId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        <u16 as Display>::fmt(&self.0, f)
+    }
+}
 
 impl NodeId {
     pub fn next(&self) -> Option<NodeId> {
