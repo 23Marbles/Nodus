f3ad539d02909e24a22fa8b75164c2061c7a288c
23 Marbles <eth8n.ryan@gmail.com>
2026-06-07 17:49:54 +0100

    Update traits

--- crates/nodus-graph/src/graph.rs
+++ crates/nodus-graph/src/graph.rs
@@ -16,22 +16,40 @@ pub trait Graph {
 }
 
 #[cfg(feature = "reflect")]
-pub trait ReflectableGraph: ReflectNodes + ReflectEdges {}
-
-impl<T: ReflectEdges + ReflectNodes> ReflectableGraph for T {}
+pub trait ReflectableGraph: ReflectNodes + ReflectEdges + CreateNodes + CreateEdges {}
 
 #[cfg(feature = "reflect")]
-pub trait ReflectNodes: Graph {
-    fn inspect_node(&mut self, id: &NodeId) -> Option<&mut dyn bevy_reflect::PartialReflect>;
+impl<T: ReflectEdges + ReflectNodes + CreateNodes + CreateEdges> ReflectableGraph for T {}
+
+#[cfg(feature = "reflect")]
+pub trait CreateNodes {
     fn default_node(&self) -> Box<dyn bevy_reflect::Reflect>;
     fn insert_node(&mut self, value: Box<dyn Reflect>) -> Result<NodeId, InsertError>;
     fn validate_node(&self, value: &dyn Reflect) -> Result<(), InsertError>;
 }
 
 #[cfg(feature = "reflect")]
-pub trait ReflectEdges: Graph {
-    fn inspect_edge(&mut self, id: &EdgeId) -> Option<&mut dyn bevy_reflect::PartialReflect>;
+pub trait ReflectNodes: Graph {
+    fn inspect_node(&mut self, id: &NodeId) -> Option<&mut dyn bevy_reflect::PartialReflect>;
+}
+
+#[cfg(feature = "reflect")]
+pub trait CreateEdges {
     fn default_edge(&self) -> Box<dyn bevy_reflect::Reflect>;
     fn insert_edge(&mut self, value: Box<dyn Reflect>) -> Result<EdgeId, InsertError>;
     fn validate_edge(&self, value: &dyn Reflect) -> Result<(), InsertError>;
+}
+
+#[cfg(feature = "reflect")]
+pub trait ReflectEdges: Graph {
+    fn inspect_edge_metadata(
+        &mut self,
+        id: &EdgeId,
+    ) -> Option<&mut dyn bevy_reflect::PartialReflect>;
+    fn edge_endpoints(&self, id: &EdgeId) -> Option<Box<dyn Iterator<Item = NodeId> + '_>>;
+    fn set_edge_endpoints(
+        &mut self,
+        id: &EdgeId,
+        new_endpoints: Box<dyn Iterator<Item = NodeId> + '_>,
+    ) -> Result<(), InsertError>;
 }
