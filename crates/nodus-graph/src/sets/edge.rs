378757e92145e643c984832d5467fbc73fcbf710
23 Marbles <eth8n.ryan@gmail.com>
2026-06-07 18:32:34 +0100

    add idgen

--- crates/nodus-graph/src/sets/edge.rs
+++ crates/nodus-graph/src/sets/edge.rs
@@ -1,4 +1,10 @@
-use std::fmt::Display;
+use std::{
+    fmt::Display,
+    sync::{
+        Mutex,
+        atomic::{AtomicU16, Ordering},
+    },
+};
 
 #[derive(Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct EdgeId(pub u16);
@@ -6,5 +12,28 @@ pub struct EdgeId(pub u16);
 impl Display for EdgeId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         <u16 as Display>::fmt(&self.0, f)
+    }
+}
+
+#[derive(Default)]
+pub struct EdgeIdPool {
+    next: AtomicU16,
+    free: Mutex<Vec<u16>>,
+}
+
+impl EdgeIdPool {
+    pub fn alloc(&self) -> EdgeId {
+        // Try to reuse an ID
+        if let Some(id) = self.free.lock().unwrap().pop() {
+            return EdgeId(id);
+        }
+
+        // Otherwise allocate a new one
+        let id = self.next.fetch_add(1, Ordering::Relaxed);
+        EdgeId(id)
+    }
+
+    pub fn free(&self, id: EdgeId) {
+        self.free.lock().unwrap().push(id.0);
     }
 }
--- crates/nodus-graph/src/sets/vertex.rs
+++ crates/nodus-graph/src/sets/vertex.rs
@@ -1,4 +1,10 @@
-use std::fmt::Display;
+use std::{
+    fmt::Display,
+    sync::{
+        Mutex,
+        atomic::{AtomicU16, Ordering},
+    },
+};
 
 #[derive(Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct NodeId(pub u16);
@@ -12,5 +18,28 @@ impl Display for NodeId {
 impl NodeId {
     pub fn next(&self) -> Option<NodeId> {
         Some(NodeId(self.0.checked_add(1)?))
+    }
+}
+
+#[derive(Default)]
+pub struct NodeIdPool {
+    next: AtomicU16,
+    free: Mutex<Vec<u16>>,
+}
+
+impl NodeIdPool {
+    pub fn alloc(&self) -> NodeId {
+        // Try to reuse an ID
+        if let Some(id) = self.free.lock().unwrap().pop() {
+            return NodeId(id);
+        }
+
+        // Otherwise allocate a new one
+        let id = self.next.fetch_add(1, Ordering::Relaxed);
+        NodeId(id)
+    }
+
+    pub fn free(&self, id: NodeId) {
+        self.free.lock().unwrap().push(id.0);
     }
 }
