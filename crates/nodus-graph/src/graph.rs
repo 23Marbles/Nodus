f40b61957241c62d52983dd3130e82e2bd6cd9b6
23 Marbles <eth8n.ryan@gmail.com>
2026-06-07 19:33:28 +0100

    update organization

--- crates/nodus-graph/src/graph.rs
+++ crates/nodus-graph/src/graph.rs
@@ -1,13 +1,5 @@
-#[cfg(feature = "reflect")]
-use bevy_reflect::Reflect;
-
 use crate::sets::{edge::EdgeId, vertex::NodeId};
 #[cfg(feature = "reflect")]
-use crate::{
-    graph::error::{EndpointRange, InsertError},
-    sets::edge::EdgeData,
-};
-
 pub mod error;
 pub mod normal;
 
@@ -18,51 +10,57 @@ pub trait Graph {
     fn incident_nodes(&self, edge_id: &EdgeId) -> Option<Box<dyn Iterator<Item = NodeId> + '_>>;
 }
 
-#[cfg(feature = "reflect")]
-pub trait ReflectableGraph: ReflectNodes + ReflectEdges + CreateNodes + CreateEdges {}
+mod reflect {
+    use crate::{
+        graph::error::{EndpointRange, InsertError},
+        sets::edge::EdgeData,
+    };
 
-#[cfg(feature = "reflect")]
-impl<T: ReflectEdges + ReflectNodes + CreateNodes + CreateEdges> ReflectableGraph for T {}
+    use bevy_reflect::Reflect;
 
-#[cfg(feature = "reflect")]
-pub trait CreateNodes {
-    fn default_node(&self) -> Box<dyn bevy_reflect::Reflect>;
-    fn insert_node(&mut self, value: Box<dyn Reflect>) -> Result<NodeId, InsertError>;
-    fn validate_node(&self, value: &dyn Reflect) -> Result<(), InsertError>;
-}
+    use super::*;
 
-#[cfg(feature = "reflect")]
-pub trait ReflectNodes: Graph {
-    fn inspect_node(&mut self, id: &NodeId) -> Option<&mut dyn bevy_reflect::PartialReflect>;
-}
+    pub trait ReflectableGraph: ReflectNodes + ReflectEdges + CreateNodes + CreateEdges {}
 
-#[cfg(feature = "reflect")]
-pub type ReflectEdgeData =
-    EdgeData<Box<dyn Iterator<Item = NodeId>>, Box<dyn bevy_reflect::Reflect>>;
+    impl<T: ReflectEdges + ReflectNodes + CreateNodes + CreateEdges> ReflectableGraph for T {}
 
-#[cfg(feature = "reflect")]
-pub type ReflectEdgeDataRef<'a> =
-    EdgeData<&'a mut dyn Iterator<Item = NodeId>, &'a dyn bevy_reflect::Reflect>;
+    pub trait CreateNodes {
+        fn default_node(&self) -> Box<dyn bevy_reflect::Reflect>;
+        fn insert_node(&mut self, value: Box<dyn Reflect>) -> Result<NodeId, InsertError>;
+        fn validate_node(&self, value: &dyn Reflect) -> Result<(), InsertError>;
+    }
 
-#[cfg(feature = "reflect")]
-pub trait CreateEdges {
-    fn default_edge(&self) -> ReflectEdgeData;
-    fn insert_edge(&mut self, value: ReflectEdgeData) -> Result<EdgeId, InsertError>;
-    fn validate_edge(&self, value: ReflectEdgeDataRef) -> Result<(), InsertError>;
-}
+    pub trait ReflectNodes: Graph {
+        fn inspect_node(&mut self, id: &NodeId) -> Option<&mut dyn bevy_reflect::PartialReflect>;
+    }
 
-#[cfg(feature = "reflect")]
-pub trait ReflectEdges: Graph {
-    fn inspect_edge(&mut self, id: &EdgeId) -> ReflectEdgeData;
-    fn inspect_edge_metadata(
-        &mut self,
-        id: &EdgeId,
-    ) -> Option<&mut dyn bevy_reflect::PartialReflect>;
-    fn endpoint_size(&self) -> EndpointRange;
-    fn edge_endpoints(&self, id: &EdgeId) -> Option<Box<dyn Iterator<Item = NodeId> + '_>>;
-    fn set_edge_endpoints(
-        &mut self,
-        id: &EdgeId,
-        new_endpoints: Box<dyn Iterator<Item = NodeId> + '_>,
-    ) -> Result<(), InsertError>;
+    pub type ReflectEdgeData =
+        EdgeData<Box<dyn Iterator<Item = NodeId>>, Box<dyn bevy_reflect::Reflect>>;
+
+    pub type ReflectEdgeDataRef<'a> =
+        EdgeData<&'a mut dyn Iterator<Item = NodeId>, &'a dyn bevy_reflect::Reflect>;
+
+    pub type ReflectEdgeDataRefMut<'a> =
+        EdgeData<&'a mut dyn Iterator<Item = NodeId>, &'a mut dyn bevy_reflect::Reflect>;
+
+    pub trait CreateEdges {
+        fn default_edge(&self) -> ReflectEdgeData;
+        fn insert_edge(&mut self, value: ReflectEdgeData) -> Result<EdgeId, InsertError>;
+        fn validate_edge(&self, value: ReflectEdgeDataRef) -> Result<(), InsertError>;
+    }
+
+    pub trait ReflectEdges: Graph {
+        fn inspect_edge(&mut self, id: &EdgeId) -> ReflectEdgeDataRefMut;
+        fn inspect_edge_metadata(
+            &mut self,
+            id: &EdgeId,
+        ) -> Option<&mut dyn bevy_reflect::PartialReflect>;
+        fn endpoint_size(&self) -> EndpointRange;
+        fn edge_endpoints(&self, id: &EdgeId) -> Option<Box<dyn Iterator<Item = NodeId> + '_>>;
+        fn set_edge_endpoints(
+            &mut self,
+            id: &EdgeId,
+            new_endpoints: Box<dyn Iterator<Item = NodeId> + '_>,
+        ) -> Result<(), InsertError>;
+    }
 }
