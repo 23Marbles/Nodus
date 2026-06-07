92121765f89797a57402c459451cdb8f0f3fc277
23 Marbles <eth8n.ryan@gmail.com>
2026-06-07 19:54:08 +0100

    implement basic graph

--- crates/nodus-graph/src/graph/normal.rs
+++ crates/nodus-graph/src/graph/normal.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-#[cfg(feature = "reflect")]
-use crate::graph::{ReflectEdges, ReflectNodes};
 use crate::{
     graph::Graph,
     sets::{edge::EdgeId, vertex::NodeId},
@@ -28,51 +26,196 @@ impl Graph for BasicGraph {
 }
 
 #[cfg(feature = "reflect")]
-impl ReflectNodes for BasicGraph {
-    fn inspect_node(&mut self, id: &NodeId) -> Option<&mut dyn bevy_reflect::PartialReflect> {
-        Some(self.nodes.get_mut(id)?)
+mod reflect_impls {
+    use std::{
+        any::{TypeId, type_name, type_name_of_val},
+        iter::repeat_n,
+        sync::LazyLock,
+    };
+
+    use crate::{
+        graph::{
+            error::{EndpointRange, InsertError},
+            reflect::{
+                CreateEdges, CreateNodes, ReflectEdgeData, ReflectEdgeDataRef, ReflectEdges,
+                ReflectNodes,
+            },
+        },
+        sets::{
+            edge::{EdgeData, EdgeIdPool},
+            vertex::NodeIdPool,
+        },
+    };
+
+    use super::*;
+
+    static NODE_ID_GEN: LazyLock<NodeIdPool> = LazyLock::new(|| NodeIdPool::default());
+
+    impl CreateNodes for BasicGraph {
+        fn default_node(&self) -> Box<dyn bevy_reflect::Reflect> {
+            Box::new(())
+        }
+
+        fn insert_node(
+            &mut self,
+            value: Box<dyn bevy_reflect::Reflect>,
+        ) -> Result<NodeId, InsertError> {
+            let name = *value
+                .downcast::<String>()
+                .map_err(|d| InsertError::WrongType {
+                    expected: type_name::<String>().to_owned(),
+                    got: type_name_of_val(d.as_any()).to_owned(),
+                })?;
+
+            let mut new_id = NODE_ID_GEN.alloc();
+
+            while self.nodes.contains_key(&new_id) {
+                NODE_ID_GEN.free(new_id);
+                new_id = NODE_ID_GEN.alloc();
+            }
+
+            self.nodes.insert(new_id, name);
+
+            Ok(new_id)
+        }
+
+        fn validate_node(&self, value: &dyn bevy_reflect::Reflect) -> Result<(), InsertError> {
+            if value.type_id() != TypeId::of::<String>() {
+                Err(InsertError::WrongType {
+                    expected: type_name::<String>().to_owned(),
+                    got: type_name_of_val(value.as_any()).to_owned(),
+                })
+            } else {
+                Ok(())
+            }
+        }
     }
 
-    fn default_node(&self) -> Box<dyn bevy_reflect::Reflect> {
-        todo!()
+    impl ReflectNodes for BasicGraph {
+        fn inspect_node(&mut self, id: &NodeId) -> Option<&mut dyn bevy_reflect::PartialReflect> {
+            Some(self.nodes.get_mut(id)?)
+        }
     }
 
-    fn insert_node(
-        &mut self,
-        value: Box<dyn bevy_reflect::Reflect>,
-    ) -> Result<NodeId, super::error::InsertError> {
-        todo!()
+    impl BasicGraph {
+        fn get_endpoints(
+            &self,
+            endpoints: impl Iterator<Item = NodeId>,
+        ) -> Result<[NodeId; 2], InsertError> {
+            let mut it = endpoints;
+
+            let end1 = it.next().ok_or(InsertError::IncorrectEndpointCount {
+                expected: EndpointRange::SingleValue(2),
+                got: 0,
+            })?;
+
+            let end2 = it.next().ok_or(InsertError::IncorrectEndpointCount {
+                expected: EndpointRange::SingleValue(2),
+                got: 1,
+            })?;
+
+            // Get remaining len
+            match it.count() {
+                0 => (),
+                x => {
+                    return Err(InsertError::IncorrectEndpointCount {
+                        expected: EndpointRange::SingleValue(2),
+                        got: x + 2,
+                    });
+                }
+            }
+
+            if !self.nodes.contains_key(&end1) {
+                return Err(InsertError::NonExistentNode { id: end1 });
+            }
+
+            if !self.nodes.contains_key(&end2) {
+                return Err(InsertError::NonExistentNode { id: end2 });
+            }
+
+            Ok([end1, end2])
+        }
     }
 
-    fn validate_node(
-        &self,
-        value: &dyn bevy_reflect::Reflect,
-    ) -> Result<(), super::error::InsertError> {
-        todo!()
-    }
-}
+    static EDGE_ID_GEN: LazyLock<EdgeIdPool> = LazyLock::new(|| EdgeIdPool::default());
 
-#[cfg(feature = "reflect")]
-impl ReflectEdges for BasicGraph {
-    fn inspect_edge(&mut self, _: &EdgeId) -> Option<&mut dyn bevy_reflect::PartialReflect> {
-        None
+    impl CreateEdges for BasicGraph {
+        fn default_edge(&self) -> ReflectEdgeData {
+            EdgeData {
+                coinident_nodes: Box::new(repeat_n(NodeId::default(), 2).into_iter()),
+                meta: Box::new(()),
+            }
+        }
+
+        fn insert_edge(&mut self, value: ReflectEdgeData) -> Result<EdgeId, InsertError> {
+            let endpoints = self.get_endpoints(value.coinident_nodes)?;
+
+            // Error check
+            value
+                .meta
+                .downcast::<()>()
+                .map_err(|d| InsertError::WrongType {
+                    expected: type_name::<()>().to_owned(),
+                    got: type_name_of_val(d.as_any()).to_owned(),
+                })?;
+
+            let mut new_id = EDGE_ID_GEN.alloc();
+
+            while self.edges.contains_key(&new_id) {
+                EDGE_ID_GEN.free(new_id);
+                new_id = EDGE_ID_GEN.alloc();
+            }
+
+            self.edges.insert(new_id, endpoints);
+
+            Ok(new_id)
+        }
+
+        fn validate_edge(&self, value: ReflectEdgeDataRef) -> Result<(), InsertError> {
+            let _ = self.get_endpoints(value.coinident_nodes)?;
+            let _ = value
+                .meta
+                .downcast_ref::<()>()
+                .ok_or(InsertError::WrongType {
+                    expected: type_name::<()>().to_owned(),
+                    got: type_name_of_val(value.meta.as_any()).to_string(),
+                })?;
+
+            Ok(())
+        }
     }
 
-    fn default_edge(&self) -> Box<dyn bevy_reflect::Reflect> {
-        todo!()
-    }
+    impl ReflectEdges for BasicGraph {
+        fn inspect_edge_metadata(
+            &mut self,
+            _: &EdgeId,
+        ) -> Option<&mut dyn bevy_reflect::PartialReflect> {
+            None
+        }
 
-    fn insert_edge(
-        &mut self,
-        value: Box<dyn bevy_reflect::Reflect>,
-    ) -> Result<EdgeId, super::error::InsertError> {
-        todo!()
-    }
+        fn endpoint_size(&self) -> EndpointRange {
+            EndpointRange::SingleValue(2)
+        }
 
-    fn validate_edge(
-        &self,
-        value: &dyn bevy_reflect::Reflect,
-    ) -> Result<(), super::error::InsertError> {
-        todo!()
+        fn edge_endpoints(&self, id: &EdgeId) -> Option<Box<dyn Iterator<Item = NodeId> + '_>> {
+            Some(Box::new(self.edges.get(id)?.clone().into_iter()))
+        }
+
+        fn set_edge_endpoints(
+            &mut self,
+            id: &EdgeId,
+            new_endpoints: Box<dyn Iterator<Item = NodeId> + '_>,
+        ) -> Result<(), InsertError> {
+            let ends = self.get_endpoints(new_endpoints)?;
+
+            let endpoints = self
+                .edges
+                .get_mut(id)
+                .ok_or(InsertError::NonExistentEdge { id: *id })?;
+
+            *endpoints = ends;
+
+            Ok(())
+        }
     }
 }
